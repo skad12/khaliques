39e8d814ff2a77ed858d3a45ea4b8c7f5cf4d44e
skad12 <stephenkunle26@gmail.com>
2024-08-09 16:24:08 +0100

    initializing

---
+++ components/shared/AppFooter.jsx
@@ -0,0 +1,68 @@
+import {
+  FiTwitter,
+  FiLinkedin,
+  FiGlobe,
+  FiYoutube,
+  FiInstagram,
+} from "react-icons/fi";
+import AppFooterCopyright from "./AppFooterCopyright";
+
+const socialLinks = [
+  {
+    id: 1,
+    icon: <FiGlobe />,
+    url: "https://www.khaliquesglobal.net",
+  },
+
+  {
+    id: 2,
+    icon: <FiTwitter />,
+    url: "",
+  },
+  {
+    id: 3,
+    icon: <FiLinkedin />,
+    url: "https://www.linkedin.com/in/khaliquesgroup",
+  },
+  {
+    id: 4,
+    icon: <FiYoutube />,
+    url: "https://www.youtube.com/@khaliquesgroup2588",
+  },
+  {
+    id: 5,
+    icon: <FiInstagram />,
+    url: "https://www.instagram.com/khaliquesgroup",
+  },
+];
+
+function AppFooter() {
+  return (
+    <div className="container mx-auto">
+      <div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
+        {/* Footer social links */}
+        <div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
+          <p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5">
+            Follow Us
+          </p>
+          <ul className="flex gap-4 sm:gap-8">
+            {socialLinks.map((link) => (
+              <a
+                href={link.url}
+                target="__blank"
+                key={link.id}
+                className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
+              >
+                <i className="text-xl sm:text-2xl md:text-3xl">{link.icon}</i>
+              </a>
+            ))}
+          </ul>
+        </div>
+
+        <AppFooterCopyright />
+      </div>
+    </div>
+  );
+}
+
+export default AppFooter;
